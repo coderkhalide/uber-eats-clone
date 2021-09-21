6c9d82971cf1285e894118bfa5de644267af8ba2
Khalid Saifullah <khalideofficial@gmail.com>
2021-09-22 01:07:41 +0600

    account screen done

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import Screen from '../components/Screen'
+import tailwind from 'tailwind-react-native-classnames';
+import AppHead from '../components/AppHead';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../redux/slices/authSlice'
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { auth } from '../configs/firebase'
 
 const AccountScreen = () => {
+    const user = useSelector(selectUser)
+    
     return (
-        <View>
-            
-        </View>
+        <Screen style={tailwind`flex-1 bg-white`}>
+            <AppHead title={`Account`} icon="settings-outline" />
+            <View style={tailwind`justify-center items-center`}>
+                <Image source={require('../assets/images/avatar.gif')} style={tailwind`w-48 h-40 rounded-full`} />
+                <Text style={tailwind`mt-4 text-3xl font-bold`}>{user?.name}</Text>
+                <Text style={tailwind`text-lg text-indigo-900`}>{user?.email}</Text>
+            </View>
+            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
+                <Text style={tailwind`text-gray-800 mt-2 text-lg mb-2`}>Saved places</Text>
+                <SavedPlaces 
+                    title="Home"
+                    text="Add home"
+                    Icon={() => <AntDesign name="home" size={24} color="black" />}
+                />
+                <SavedPlaces 
+                    title="Word"
+                    text="Add work"
+                    Icon={() => <Ionicons name="md-briefcase-outline" size={24} color="black" />}
+                />
+            </View>
+            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
+                <Text style={tailwind`text-gray-800 mt-2 text-lg`}>Other options</Text>
+                <TouchableOpacity onPress={() => auth.signOut()}>
+                    <Text style={tailwind`text-green-900 mt-2`}>Sign out</Text>
+                </TouchableOpacity>
+            </View>
+        </Screen>
     );
 }
 
-const styles = StyleSheet.create({})
-
 export default AccountScreen;
+
+const SavedPlaces = ({ title, text, Icon }) => (
+    <TouchableOpacity style={tailwind`flex-row items-center my-3`}>
+        <Icon />
+        <View style={tailwind`ml-5`}>
+            <Text style={tailwind`text-gray-800`}>{title}</Text>
+            <Text style={tailwind`text-gray-600 text-xs mt-1`}>{text}</Text>
+        </View>
+    </TouchableOpacity>
+)
