ff22ac4e9ed82c14926e5f6b0f71701598c1a2e3
Khalid Saifullah <khalideofficial@gmail.com>
2022-07-31 21:03:34 +0600

    Few improvement

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -5,6 +5,8 @@ import { loginUser, logoutUser, selectUser } from '../redux/slices/authSlice';
 import AuthNavigator from './AuthNavigator';
 import { auth } from '../configs/firebase';
 import HomeNavigator from './HomeNavigator';
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['new NativeEventEmitter']);
 
 export default function AppNavigator() {
     const user = useSelector(selectUser)
